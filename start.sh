35e3574b12539bef9a4a260379ed5dc40fecd418
SynthAI139 <torinriley220+4@gmail.com>
2025-06-03 14:53:19 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "[+] Starting Monero mining to wallet..."
-/xmrig/build/xmrig -o pool.supportxmr.com:443 -u 84hRJwkVMPDSXZ3YaW5EfWgrZX96EzBQbgDYTwSZAQpqGzTQfw9u8Mg1E1BqJwEHeqc8p4nAZiXjqb7cWWJSWg3SNjRpDrq -k --tls --cpu-priority=4 --donate-level=1
+echo "[+] Starting MoneroOcean mining to wallet..."
+/xmrig/build/xmrig \
+  -o gulf.moneroocean.stream:10128 \
+  -u 84hRJwkVMPDSXZ3YaW5EfWgrZX96EzBQbgDYTwSZAQpqGzTQfw9u8Mg1E1BqJwEHeqc8p4nAZiXjqb7cWWJSWg3SNjRpDrq \
+  --coin monero \
+  --cpu-priority=4 \
+  --donate-level=1
